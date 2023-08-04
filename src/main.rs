6db9556243d34f976328be89b78306c56bad5686
Andreas Lindhé <andreas@lindhe.io>
2023-08-04 22:08:12 +0200

    Create stub for returning emojis

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/emoji/<id>")]
+fn emoji(id: &str) -> String {
+    format!("TODO: Return emoji with ID {}. 🙃", id)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![emoji])
 }
